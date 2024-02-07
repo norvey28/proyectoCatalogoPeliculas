dee3eb2b72687779046a6ddba22f0d927cda43ae
Norvey Alfonso Peña Romero <norveyalfonso28@gmail.com>
2024-02-06 19:45:00 -0500

    REVISAR Diagramas de UML para saber cuando se extiende y cuando se implementa
    CORRECCION Se extienden las clases hijas de AccesoDatosEx y se agregan sus constructores

--- src/mx/com/gm/peliculas/excepciones/EscrituraDatosEx.java
+++ src/mx/com/gm/peliculas/excepciones/EscrituraDatosEx.java
@@ -4,6 +4,10 @@ package mx.com.gm.peliculas.excepciones;
  *
  * @author NORVEY
  */
-public class EscrituraDatosEx {
+public class EscrituraDatosEx extends AccesoDatosEx{
+
+    public EscrituraDatosEx(String message) {
+        super(message);
+    }
     
 }
--- src/mx/com/gm/peliculas/excepciones/LecturaDatosEx.java
+++ src/mx/com/gm/peliculas/excepciones/LecturaDatosEx.java
@@ -4,6 +4,10 @@ package mx.com.gm.peliculas.excepciones;
  *
  * @author NORVEY
  */
-public class LecturaDatosEx {
+public class LecturaDatosEx extends AccesoDatosEx{
+
+    public LecturaDatosEx(String message) {
+        super(message);
+    }
     
 }
