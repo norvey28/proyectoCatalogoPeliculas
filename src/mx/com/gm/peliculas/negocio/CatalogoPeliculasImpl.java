1c7afa6b98965b2428a275cde66f10f33627e4b6
Norvey Alfonso Peña Romero <norveyalfonso28@gmail.com>
2024-02-10 14:19:17 -0500

    Feat Se culmina configuracion archivo CatalogoPeliculasImpl

--- src/mx/com/gm/peliculas/negocio/CatalogoPeliculasImpl.java
+++ src/mx/com/gm/peliculas/negocio/CatalogoPeliculasImpl.java
@@ -6,6 +6,7 @@ import mx.com.gm.peliculas.datos.AccesoDatos;
 import mx.com.gm.peliculas.datos.AccesoDatosImpl;
 import mx.com.gm.peliculas.domain.Pelicula;
 import mx.com.gm.peliculas.excepciones.AccesoDatosEx;
+import mx.com.gm.peliculas.excepciones.LecturaDatosEx;
 
 /**
  *
@@ -29,20 +30,49 @@ public class CatalogoPeliculasImpl implements CatalogoPeliculas {
 
         } catch (AccesoDatosEx ex) {
             System.out.println("Error de acceso a Datos");
-            ex.printStackTrace();
+            ex.printStackTrace(System.out);
         }
     }
 
     @Override
     public void listarPeliculas(String nombreArchivo) {
+        try {
+            var peliculas = this.datos.listar(NOMBRE_RECURSO);
+            for(var pelicula : peliculas){
+                System.out.println("pelicula = " + pelicula);
+            }
+        } catch (AccesoDatosEx ex) {
+            System.out.println("Error de acceso a Datos");
+            ex.printStackTrace(System.out);
+        }
     }
 
     @Override
     public void buscarPelicula(String nombreArchivo, String buscar) {
+        
+        String resultado = null;
+        try {
+            resultado = this.datos.buscar(NOMBRE_RECURSO, buscar);
+        } catch (LecturaDatosEx ex) {
+            System.out.println("Error de acceso a datos");
+            ex.printStackTrace();
+        }
+        System.out.println("resultado = " + resultado);
     }
 
     @Override
     public void inicarArchivo(String nombreArchivo) {
+        try {
+            if(datos.existe(NOMBRE_RECURSO)){
+                datos.borrar(NOMBRE_RECURSO);
+                datos.crear(NOMBRE_RECURSO);
+            }else{
+                datos.crear(NOMBRE_RECURSO);
+            }
+        } catch (AccesoDatosEx ex) {
+            System.out.println("Eror al iniciar catalogo de peliculas");
+            ex.printStackTrace();
+        }
     }
 
 }
